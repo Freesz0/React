f44f6e26f4fa663f20da0adb17fd5fd48f03c7cf
Freesz0 <matheusfreesz1@gmail.com>
2025-02-24 17:31:14 -0300

    inline-block pra alinhar a data (inl-blk) com descrição (inline)
    separar classe Resumo de Descricao
    
    ajuste de margem e padding (Descricao 390 -> 110)

--- aula-dfe/src/App.js
+++ aula-dfe/src/App.js
@@ -16,15 +16,21 @@ function App() {
         </p>
         </div>
       </header>
-      <div className = "Barra"><h2 className = "DivTitulo">Resumo</h2>
-        <p className = "Descricao">
+
+      <div className = "Barra">
+        <h2 className = "DivTitulo">Resumo</h2>
+        <p className = "Resumo">
           Atual campeão da divisão meio-médio do UFC e ex-campeão da divisão média, faixa preta em Kickboxing e Jiu-jitsu brasileiro. Defendi meu cinturão de oponentes mais ágeis 5 vezes, todas com nocautes e nocautes técnicos. Atualmente 3º colocado nos top 15 Pound-for-Pound, classificação que determina o sucesso e desempenho dos lutadores da organização como um todo, sem considerar pesagem.
         </p>
       </div>
-      <div className = "Barra"><h2 className = "DivTitulo">Experiência Profissional</h2>
-        <p className="Descricao">
-          
-        </p>
+
+      
+      <div className = "Barra">
+        <h2 className = "DivTitulo">Experiência Profissional</h2>
+        <div className = "Data">2010-2021</div>
+        <div className = "Descricao">
+        Vencedor do <em>Glory 14: Zagreb - Middleweight Contender Tournament</em> no dia 08 de Março de 2014.
+        </div>
       </div>
     </div>
   );
