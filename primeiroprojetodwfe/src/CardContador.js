b374409fb62882e2cabfd76cf1358b4cd6d2c1a2
Matheus <matheusfreesz1@gmail.com>
2025-03-17 17:53:12 -0300

    Contador de pontos, botão Zerar agora apaga Commits e Pontos

--- primeiroprojetodwfe/src/CardContador.js
+++ primeiroprojetodwfe/src/CardContador.js
@@ -6,10 +6,11 @@ import Card from 'react-bootstrap/Card';
 
 function CardContador() {
     const [contagem, setContagem] = useState(0)
+    const [pontos, setPontos] = useState(0)
 
     useEffect(() => {
-      if (contagem === 10) {
-        apresentarAlerta()
+      if (contagem % 10 === 0 && contagem !== 0) {
+        apresentarAlerta(setPontos(pontos + 2))
       }
     }, [contagem]);
 
@@ -20,11 +21,11 @@ function CardContador() {
     const apresentarAlerta = () => {
       alert('Você deu 10 commits, tome aqui 2 pontos!')
     }
-
+/*
     const alertaZerar = () => {
       alert('Você tem certeza que deseja zerar seus commits?')
     }
-    
+*/
 
     return (
     <Card style={{ width: '30rem' }}>
@@ -32,12 +33,14 @@ function CardContador() {
       <Card.Body>
         <Card.Title className="CardTitle">Simulador de Commit</Card.Title>
         <Card.Text className="CardText">
-          Commits: {contagem}
+          Commits: {contagem}<br />
+          Pontos: {pontos}
         </Card.Text>
         <Card.Body>
           <div className="ButtonCard">
             <Button style={{fontSize:'1.5rem', fontWeight: 900}} variant="success" onClick={apresentaMenu}>Dar Commit</Button>
-            <Button style={{fontSize:'1.5rem'}} variant="danger" onClick={() => setContagem(0)}>Zerar</Button>
+            <Button style={{fontSize:'1.5rem'}} variant="danger" onClick={() => { setContagem(0); setPontos(0); }}>Zerar</Button>
+           
           </div>
         </Card.Body>
       </Card.Body>
