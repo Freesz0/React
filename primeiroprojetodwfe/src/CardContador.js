a4f503ca48732fd63a270856e78080cc8e1c0531
Matheus <matheusfreesz1@gmail.com>
2025-03-17 18:52:15 -0300

    Substituilção de alerta por Modal ao zerar commits e pontos

--- primeiroprojetodwfe/src/CardContador.js
+++ primeiroprojetodwfe/src/CardContador.js
@@ -2,11 +2,12 @@ import './CardContador.css';
 import React, { useEffect, useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
+import Modal from 'react-bootstrap/Modal';
 
 function CardContador() {
     const [contagem, setContagem] = useState(0)
     const [pontos, setPontos] = useState(0)
+    const [showModal, setShowModal] = useState(false)
 
     useEffect(() => {
       if (contagem % 10 === 0 && contagem !== 0) {
@@ -21,13 +22,13 @@ function CardContador() {
     const apresentarAlerta = () => {
       alert('Você deu 10 commits, tome aqui 2 pontos!')
     }
-/*
+
     const alertaZerar = () => {
-      alert('Você tem certeza que deseja zerar seus commits?')
-    }
-*/
+      setShowModal(true)};
 
     return (
+
+      
     <Card style={{ width: '30rem' }}>
       <Card.Img variant="top" src="/CardIcon.png" alt="Logo" />
       <Card.Body>
@@ -37,11 +38,31 @@ function CardContador() {
           Pontos: {pontos}
         </Card.Text>
         <Card.Body>
-          <div className="ButtonCard">
-            <Button style={{fontSize:'1.5rem', fontWeight: 900}} variant="success" onClick={apresentaMenu}>Dar Commit</Button>
-            <Button style={{fontSize:'1.5rem'}} variant="danger" onClick={() => { setContagem(0); setPontos(0); }}>Zerar</Button>
+          <div className="d-grid gap-2">
+            <Button size="lg" style={{fontSize:'1.5rem', fontWeight: 900}} variant="success" onClick={apresentaMenu}>
+              DAR COMMIT
+            </Button>
+            <Button size="sm" style={{fontSize:'1rem'}} variant="danger" onClick={alertaZerar}>
+              ZERAR
+            </Button>
           </div>
         </Card.Body>
+        <Modal show={showModal} onHide={() => setShowModal(false)}>
+            <Modal.Header closeButton>
+                <Modal.Title>CUIDADO!</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                Você tem certeza que deseja zerar seus commits e pontos?
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={() => setShowModal(false)}>
+                    Cancelar
+                </Button>
+                <Button variant="danger" onClick={() => { setContagem(0); setPontos(0); setShowModal(false); }}>
+                    Zerar
+                </Button>
+            </Modal.Footer>
+        </Modal>
       </Card.Body>
     </Card>
     );
